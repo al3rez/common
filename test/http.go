eddcaa61d308f3bf53289cf93b658492d6fbf1c3
Alireza Bashiri <azbshiri@gmail.com>
2018-09-05 12:38:37 +0430

    Abstract out server concept with http.Handler

--- test/http.go
+++ test/http.go
@@ -6,12 +6,12 @@ import (
 	"net/http/httptest"
 )
 
-func DoRequest(server *server, method, path string, body io.Reader) (*httptest.ResponseRecorder, error) {
+func DoRequest(handler http.Handler, method, path string, body io.Reader) (*httptest.ResponseRecorder, error) {
 	req, err := http.NewRequest(method, path, body)
 	if err != nil {
 		return nil, err
 	}
 	res := httptest.NewRecorder()
-	server.mux.ServeHTTP(res, req)
+	handler.ServeHTTP(res, req)
 	return res, nil
 }
